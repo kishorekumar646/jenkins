8fc7decf9a66f24945fa5b51d766ce9b79f6e5ea
kishorekumar646 <kishorekumar131646@gmail.com>
2020-05-09 06:54:51 +0530

    [kishore]Add.added flask_demo.py file

---
+++ flask_demo.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__,)
+
+@app.route('/')
+def test():
+    
+    return "<h1 style='color:green'>Hi hello flask, How are you?</h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
